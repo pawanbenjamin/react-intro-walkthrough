fc5ecbdc70a8fc8abf4ec12936bed0f8d66f35c6
pawanbenjamin <pawanbenjamin@gmail.com>
2021-07-08 22:20:58 -0400

    bug in poke object

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,18 +2,13 @@ import React, { useEffect, useState } from "react";
 
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState([]);
+
   async function getPokemon(e) {
     e.preventDefault();
     let pokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
     let { results } = await pokemons.json();
-    let newRes = results.map(async (result) => {
-      let response = await fetch(result.url);
-      let goodStuff = await response.json();
-      return goodStuff;
-    });
-    console.log(newRes);
+    console.log(results);
     setPokemon(results);
-    console.log(pokemon);
   }
 
   return (
